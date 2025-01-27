7e6c72168fed96ea756b8a76c1f0cf5fd32d53e9
iBowler1995 <93746898+iBowler1995@users.noreply.github.com>
2025-01-26 21:49:49 -0500

    Update and rename Generate-WeeklyDinners - Clean.ps1 to Generate-WeeklyDinners.ps1

--- Generate-WeeklyDinners.ps1
+++ Generate-WeeklyDinners.ps1
@@ -217,24 +217,40 @@ Function Get-Dinners {
         [String]$JsonPath
     )
 
-    #Determining the past dinner selections, to avoid repeats
-    $existingNumbers = Get-Content $filePath | ForEach-Object { [int]$_ }
-
-    #Loading dinner JSON and counting dinners
-    $dinners = Get-Content -Raw -Path $JsonPath | ConvertFrom-Json
-    $totalDinners = $dinners.Count
-
-    #Generating unique random numbers not already in the file, using the number of dinners. JSON is base 0, so the first dinner in the list is 0 in the index
-    $randomNumbers = @()
-    while ($randomNumbers.Count -lt $DinnerCount) {
-        $newNumber = Get-Random -Minimum 0 -Maximum $totalDinners
-        if ($newNumber -notin $existingNumbers -and $newNumber -notin $randomNumbers) {
-            $randomNumbers += $newNumber
-        }
+    Try{
+        #Determining the past dinner selections, to avoid repeats
+        $existingNumbers = Get-Content $filePath | ForEach-Object { [int]$_ }
+    }
+    Catch {
+        Write-Host "Error at line $($_.InvocationInfo.ScriptLineNumber) while getting past dinners: $($_.Exception.Message)"
     }
 
-    #Adding the new numbers to our count
-    $randomNumbers | ForEach-Object { $_ } | Add-Content -Path $filePath
+    Try{
+        #Loading dinner JSON and counting dinners
+        $dinners = Get-Content -Raw -Path $JsonPath | ConvertFrom-Json
+        $totalDinners = $dinners.Count
+    }
+    Catch {
+        Write-Host "Error at line $($_.InvocationInfo.ScriptLineNumber) while loading dinner JSON: $($_.Exception.Message)"
+    }
+
+    Try{
+        #Generating unique random numbers not already in the file, using the number of dinners. JSON is base 0, so the first dinner in the list is 0 in the index
+        $randomNumbers = @()
+        while ($randomNumbers.Count -lt $DinnerCount) {
+            $newNumber = Get-Random -Minimum 0 -Maximum $totalDinners
+            if ($newNumber -notin $existingNumbers -and $newNumber -notin $randomNumbers) {
+                $randomNumbers += $newNumber
+            }
+        }
+        #Adding the new numbers to our count
+        $randomNumbers | ForEach-Object { $_ } | Add-Content -Path $filePath
+    }
+    Catch {
+        Write-Host "Error at line $($_.InvocationInfo.ScriptLineNumber) while generating new dinners or updating used dinners: $($_.Exception.Message)"
+    }
+
+    
 
     #Checking number of dinner selections. After 15, we allow the first 5 to go back into rotation
     if ($existingNumbers.Count -ge 15) {
@@ -245,25 +261,31 @@ Function Get-Dinners {
     #Getting meals corresponding with the generated numbers
     $selectedDinners = $randomNumbers | ForEach-Object { $dinners[$_].Name }
 
-    #Determining if pulled pork has been selected. If so, we need to make sure there are two pulled pork dinners
-    $pulledPorkDinners = $dinners | Where-Object { $_.Tag -eq "PulledPork" }
-    $selectedPulledPork = $selectedDinners | Where-Object { $pulledPorkDinners.Name -contains $_ }
+    Try{
+        #Determining if pulled pork has been selected. If so, we need to make sure there are two pulled pork dinners
+        $pulledPorkDinners = $dinners | Where-Object { $_.Tag -eq "PulledPork" }
+        $selectedPulledPork = $selectedDinners | Where-Object { $pulledPorkDinners.Name -contains $_ }
 
-    if ($selectedPulledPork.Count -eq 1) {
-        $remainingPulledPork = $pulledPorkDinners | Where-Object { $selectedPulledPork -notcontains $_.Name }
-        if ($remainingPulledPork.Count -gt 0) {
-            $additionalPulledPork = $remainingPulledPork | Get-Random
-            Write-Host "Adding an additional pulled pork dinner: $($additionalPulledPork.Name)"
+        if ($selectedPulledPork.Count -eq 1) {
+            $remainingPulledPork = $pulledPorkDinners | Where-Object { $selectedPulledPork -notcontains $_.Name }
+            if ($remainingPulledPork.Count -gt 0) {
+                $additionalPulledPork = $remainingPulledPork | Get-Random
+                Write-Host "Adding an additional pulled pork dinner: $($additionalPulledPork.Name)"
 
-            #Replacing a random non-pulled pork dinner with the additional pulled pork dinner
-            $nonPulledPorkDinners = $selectedDinners | Where-Object { $pulledPorkDinners.Name -notcontains $_ }
-            if ($nonPulledPorkDinners.Count -gt 0) {
-                $dinnerToReplace = $nonPulledPorkDinners | Get-Random
-                $selectedDinners[$selectedDinners.IndexOf($dinnerToReplace)] = $additionalPulledPork.Name
-                Write-Host "Replaced dinner: $dinnerToReplace with $($additionalPulledPork.Name)"
+                #Replacing a random non-pulled pork dinner with the additional pulled pork dinner
+                $nonPulledPorkDinners = $selectedDinners | Where-Object { $pulledPorkDinners.Name -notcontains $_ }
+                if ($nonPulledPorkDinners.Count -gt 0) {
+                    $dinnerToReplace = $nonPulledPorkDinners | Get-Random
+                    $selectedDinners[$selectedDinners.IndexOf($dinnerToReplace)] = $additionalPulledPork.Name
+                    Write-Host "Replaced dinner: $dinnerToReplace with $($additionalPulledPork.Name)"
+                }
             }
         }
     }
+    Catch {
+        Write-Host "Error at line $($_.InvocationInfo.ScriptLineNumber) while getting seecond pulled pork dinner: $($_.Exception.Message)"
+    }
+
 
     #Returning the selected dinners
     return $selectedDinners
@@ -280,61 +302,76 @@ Function Get-Ingredients {
         [string]$JsonFile
     )
 
-    #Loading the JSON file
-    $dinners = Get-Content -Raw -Path $JsonFile | ConvertFrom-Json
+    try{
+        #Loading the JSON file
+        $dinners = Get-Content -Raw -Path $JsonFile | ConvertFrom-Json
+    }
+    catch {
+        Write-Host "Error at line $($_.InvocationInfo.ScriptLineNumber) while loading the JSON file: $($_.Exception.Message)"
+    }
 
-    #Creating a lookup hashtable for faster access
-    $dinnerLookup = @{}
-    foreach ($dinner in $dinners) {
-        $dinnerLookup[$dinner.Name] = $dinner
+    try{
+        #Creating a lookup hashtable for faster access
+        $dinnerLookup = @{}
+        foreach ($dinner in $dinners) {
+            $dinnerLookup[$dinner.Name] = $dinner
+        }
+    }
+    catch {
+        Write-Host "Error at line $($_.InvocationInfo.ScriptLineNumber) while creating the lookup table: $($_.Exception.Message)"
     }
 
     #Initializng hash tables for main and staple ingredients
     $aggregatedIngredients = @{}
     $aggregatedStaples = @{}
 
-    #Looping through each selected dinner
-    foreach ($dinner in $SelectedDinners) {
-        Write-Output "Processing Dinner: $dinner"
+    try{
+        #Looping through each selected dinner
+        foreach ($dinner in $SelectedDinners) {
+            Write-Output "Processing Dinner: $dinner"
 
-        #Getting corresponding dinner
-        if ($dinnerLookup.ContainsKey($dinner)) {
-            $row = $dinnerLookup[$dinner]
+            #Getting corresponding dinner
+            if ($dinnerLookup.ContainsKey($dinner)) {
+                $row = $dinnerLookup[$dinner]
 
-            #Looping through ingredients
-            foreach ($ingredient in $row.Ingredients) {
-                $name = $ingredient.Name
-                $quantity = $ingredient.Quantity -as [double]
-                $unit = $ingredient.Unit
+                #Looping through ingredients
+                foreach ($ingredient in $row.Ingredients) {
+                    $name = $ingredient.Name
+                    $quantity = $ingredient.Quantity -as [double]
+                    $unit = $ingredient.Unit
 
-                #Assigning defaults for quantity and units. This is more of a quirk for me that I added. If I don't specify quantity it assumes a quantity of 1, if I don't specify a unit it sets unit to Null/empty to avoid weird errors.
-                if (-not $quantity -or $quantity -eq $null) { $quantity = 1 }
-                if (-not $unit) { $unit = "" }
+                    #Assigning defaults for quantity and units. This is more of a quirk for me that I added. If I don't specify quantity it assumes a quantity of 1, if I don't specify a unit it sets unit to Null/empty to avoid weird errors.
+                    if (-not $quantity -or $quantity -eq $null) { $quantity = 1 }
+                    if (-not $unit) { $unit = "" }
 
-                #Aggregating staple and regular ingredients
-                if ($ingredient.Staple -eq $true) {
-                    if ($aggregatedStaples.ContainsKey($name)) {
-                        $aggregatedStaples[$name].Quantity += $quantity
-                    } else {
-                        $aggregatedStaples[$name] = @{
-                            Quantity = $quantity
-                            Unit = $unit
+                    #Aggregating staple and regular ingredients
+                    if ($ingredient.Staple -eq $true) {
+                        if ($aggregatedStaples.ContainsKey($name)) {
+                            $aggregatedStaples[$name].Quantity += $quantity
+                        } else {
+                            $aggregatedStaples[$name] = @{
+                                Quantity = $quantity
+                                Unit = $unit
+                            }
                         }
-                    }
-                } else {
-                    if ($aggregatedIngredients.ContainsKey($name)) {
-                        $aggregatedIngredients[$name].Quantity += $quantity
                     } else {
-                        $aggregatedIngredients[$name] = @{
-                            Quantity = $quantity
-                            Unit = $unit
+                        if ($aggregatedIngredients.ContainsKey($name)) {
+                            $aggregatedIngredients[$name].Quantity += $quantity
+                        } else {
+                            $aggregatedIngredients[$name] = @{
+                                Quantity = $quantity
+                                Unit = $unit
+                            }
                         }
                     }
                 }
+            } else {
+                Write-Output "No match found for: $dinner"
             }
-        } else {
-            Write-Output "No match found for: $dinner"
         }
+    }
+    catch {
+        Write-Host "Error at line $($_.InvocationInfo.ScriptLineNumber) while processing dinner '$dinner': $($_.Exception.Message)"
     }
 
     #Formatting the ingredient list
